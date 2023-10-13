479201e99b1df2c992f973b0d35bc64fcc875a9b
Tanatip Denduangchai <tanatip@siamsquared.com>
2023-10-13 17:10:56 +0700

    Refactor HomeViewController and Home.storyboard
    - Renamed ViewController.swift to HomeViewController.swift
    - Updated references to ViewController in project.pbxproj
    - Updated customClass attribute in Home.storyboard from ViewController to HomeViewController

--- Cinemo App/Cinemo App/Scene/Home/HomeViewController.swift
+++ Cinemo App/Cinemo App/Scene/Home/HomeViewController.swift
@@ -5,4 +5,15 @@
 //  Created by Tanatip Denduangchai on 10/13/23.
 //
 
-import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
